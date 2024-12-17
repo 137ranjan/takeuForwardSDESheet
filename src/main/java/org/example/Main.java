065f8262dc0b5232dfaa54ad55165a24a3c7e9d5
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-12-17 22:53:42 +0530

    Testing again

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         int[][] grid = {
                 {2,1,1},
-                {1,1,1},
-                {0,1,1}
+                {0,0,1},
+                {1,1,1}
         };
         RottenOranges solution = new RottenOranges();
         System.out.println(solution.orangesRotting(grid));
