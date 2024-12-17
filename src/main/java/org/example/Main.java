53561a5498f042f7adf3c878c0e4f9495251ac5a
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-12-17 22:32:06 +0530

    Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ import org.example.stackqueue.RottenOranges;
 public class Main {
     public static void main(String[] args) {
         int[][] grid = {
-                {0}
+                {2,1,1},
+                {0,0,1},
+                {1,0,1}
         };
         RottenOranges solution = new RottenOranges();
         System.out.println(solution.orangesRotting(grid));
